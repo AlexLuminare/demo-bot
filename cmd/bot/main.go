353f7a1f22d4ae0a8b67796d31723d39a44d5c0d
Alex Yersh <yersh.alex@gmail.com>
2025-04-22 15:34:03 +0300

    added basic functionality

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("It works!!!")
+
+	Token := os.Getenv("TELEGRAM_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(Token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Optional: wait for updates and clear them if you don't want to handle
+	// a large backlog of old messages
+	time.Sleep(time.Millisecond * 500)
+	updates.Clear()
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		// Add logic here
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+		bot.Send(msg)
+
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/AlexLuminare/demo-bot
 
 go 1.24.0
+
+require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
